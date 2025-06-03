35bf49515ca5807b68e3367c0752cd0e3765e97d
fxrocketpips1 <ayodejiakinola64@gmail.com>
2025-06-03 11:20:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,3 +64,8 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
